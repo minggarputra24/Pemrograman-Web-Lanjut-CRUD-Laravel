7209778ecea3cf1780952f01264d21f1f36ecbd9
minggarputra24 <minggarputra24@gmail.com>
2020-04-13 12:56:46 +0700

    PWL_JS 11
    Pemrograman Web Lanjut-CRUD Laravel

---
+++ laravel-crud/resources/views/profil.blade.php
@@ -0,0 +1,10 @@
+<!DOCTOTYPE html>
+<html>
+<head>
+        <title>Profil Saya</title>
+</head>
+<body>
+        <h1>Profil Saya</h1>
+        <p>Perkenalkan Nama saya Martin Amanu Khusna, saya seorang mahasiswa Polinema </p>
+</body>
+</html>
